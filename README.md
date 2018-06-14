8e4b2501731bbcc169858d00f6f1fbde62ade353
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-06-14 19:41:30 +0530

    basically everything

---
+++ README.md
@@ -0,0 +1,47 @@
+<p align="center">
+  <img src="https://78.media.tumblr.com/240d51485c042ab5bed754294ad020c6/tumblr_msz10zJc751svwlszo2_500.gif" height="200px"/>
+  <br><br>
+  <b>Interactive mode for <a target="blank" href="https://github.com/kentcdodds/nps">nps</a></b>
+</p>
+
+&nbsp;
+
+#### install
+
+```
+npm install nps-i
+```
+
+or install it globally
+
+```
+npm install -g nps-i
+```
+
+&nbsp;
+
+#### usage
+
+Replace `nps` with `nps-i` in your package.json
+
+```json
+{
+  "scripts": {
+    "start": "nps-i"
+  }
+}
+```
+
+You can also use it with the shorthand `ni` when installed globally.
+
+&nbsp;
+
+#### like it?
+
+:star: this repo
+
+&nbsp;
+
+#### license
+
+MIT © [siddharthkp](https://github.com/siddharthkp)
---
+++ index.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+
+const inquirer = require('inquirer')
+const autocomplete = require('inquirer-autocomplete-prompt')
+const fuzzy = require('fuzzy')
+const pad = require('right-pad')
+const exec = require('./exec')
+
+const packageScripts = require('./test-package-scripts.js').scripts
+
+let flatScripts = []
+
+const flattenScripts = (scripts, prefix) => {
+  const keys = Object.keys(scripts)
+  keys.forEach(key => {
+    // format = name: command
+    let script
+    let description
+    let name
+
+    if (prefix) name = prefix + '.' + key
+    else name = key
+
+    // format = name: command
+    if (typeof scripts[key] === 'string') {
+      script = scripts[key]
+      description = ''
+    }
+
+    if (typeof scripts[key] === 'object') {
+      const shape = scripts[key]
+
+      // format = name: { default: command }
+      if (typeof shape.default === 'string') {
+        script = shape.default
+        description = shape.description
+
+        delete shape.default
+        delete shape.description
+      }
+
+      // format = name: { script: command }
+      if (typeof shape.script === 'string') {
+        script = shape.script
+        description = shape.description
+
+        delete shape.script
+        delete shape.description
+      }
+
+      // recursively call for other shapes inside this object
+      // format = parent: { child: { script: command } }
+      flattenScripts(shape, name)
+    }
+
+    if (script) flatScripts.push({ name, script, description })
+  })
+}
+
+/* Flatten scripts */
+flattenScripts(packageScripts)
+
+/* Find longest key */
+let longestKey = ''
+flatScripts.forEach(element => {
+  if (element.name.length > longestKey.length) longestKey = element.name
+})
+
+/* Width of key column */
+const width = longestKey.length + 5
+
+/* Add pretty string to each element */
+flatScripts = flatScripts.map(element => {
+  element.prettyString = `${pad(element.name, width)} ${element.description}`
+  return element
+})
+
+const fuzzyOptions = {
+  extract: element => element.prettyString
+}
+
+const filterScripts = (_, input) => {
+  input = input || ''
+
+  return new Promise(resolve => {
+    const results = fuzzy.filter(input, flatScripts, fuzzyOptions)
+    const prettyResults = results.map(result => {
+      return result.original.prettyString
+    })
+    resolve(prettyResults)
+  })
+}
+
+const autocompleteOptions = {
+  type: 'autocomplete',
+  name: 'string',
+  message: 'Which script would you like to run?\n\n',
+  source: filterScripts
+}
+
+inquirer.registerPrompt('autocomplete', autocomplete)
+inquirer.prompt(autocompleteOptions).then(result => {
+  const element = flatScripts.find(element => element.prettyString === result.string)
+  exec(`nps ${element.name}`)
+})
